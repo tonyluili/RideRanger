be24e89350e91e3aac75454ec2c634cd1447bb1e
Count-T <tony.lilui416@gmail.com>
2023-01-08 09:34:40 -0500

    finished google maps api logic, destination, distance, duration

--- frontend/src/Components/LoginPage.js
+++ frontend/src/Components/LoginPage.js
@@ -1,71 +1,98 @@
 import React from "react";
-import {useRef, useState, useEffect, useContext} from "react";
+import { useRef, useState, useEffect, useContext } from "react";
 import AuthContext from "../context/AuthProvider"
-import axios, { isCancel, AxiosError } from "axios";
+import axios from "../api/axios";
+const LOGIN_URL = "/auth";
 function LoginPage() {
-  const {setAuth}  = useContext(AuthContext);
+  const { setAuth } = useContext(AuthContext);
   const userRef = useRef();
   const errRef = useRef();
 
-const [user, setUser] = useState("");
-const [password, setPassword] = useState("");
-const [errorMessage, setErrorMessage] = useState("");
-const [success, setSuccess] = useState(false);
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [success, setSuccess] = useState(false);
 
-  useEffect(() =>{
+  useEffect(() => {
     userRef.current.focus();
-  },[]);
+  }, []);
 
-  useEffect(() =>{
+  useEffect(() => {
     setErrorMessage("");
-  },[user,password]);
-  
-  const handleSubmit = async (e) =>{
+  }, [user, password]);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(user, password)
-    setUser("");
-    setPassword("");
-    setSuccess(true);
+
+    try {
+      const response = await axios.post(LOGIN_URL, JSON.stringify({ user, password }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true
+        })
+        console.log(JSON.stringify(response?.data));
+        const accessToken = response?.data?.accessToken;
+        const roles = response?.data?.roles;
+        setAuth({user, password, roles, accessToken})
+      setUser("");
+      setPassword("");
+      setSuccess(true);
+
+    } catch (err) {
+      if(!err?.response){
+        setErrorMessage("No Server Response");
+      }
+      else if(err.response?.status === 400){
+        setErrorMessage("Missing Username or Password");
+      }
+      else if(err.response?.status === 401){
+        setErrorMessage("Unauthorized");
+      }
+      else{
+        setErrorMessage("Login Failed")
+      }
+      errRef.current.focus();
+    }
   }
 
 
   return (
     <>
-     {success ? (
-      <div>
-        <h1>Success you are logged in!</h1>
-        <br />
-        <p>
-          <a href="#"> Go to home</a>
-        </p>
+      {success ? (
+        <div>
+          <h1>Success you are logged in!</h1>
+          <br />
+          <p>
+            <a href="#"> Go to home</a>
+          </p>
         </div>
-     ) : (
-      <div>
-      <h1>Sign In</h1>
-      <form onSubmit = {handleSubmit}>
-        <label htmlFor="username">Username:</label>
-        <input type="text"  id="username" ref = {userRef} autocomplete = "off" onChange={(e) =>{
-          setUser(e.target.value)
-        }}
-        
-        value = {user}
-        required />
+      ) : (
+        <div>
+          <h1>Sign In</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" ref={userRef} autocomplete="off" onChange={(e) => {
+              setUser(e.target.value)
+            }}
 
-        <label htmlFor="password">Password:</label>
-        <input type="password"  id="password"  onChange={(e) =>{
-          setPassword(e.target.value)
-        }}
-        
-        value = {password}
-        required />
-        <button>Sign In</button>
-      </form>
-      <p>Need an Account? <br /> <span className="line">{
-        <a href="#">Sign Up</a>
-      }</span></p>
-    </div>
-     )}
-  </>)
+              value={user}
+              required />
+
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" onChange={(e) => {
+              setPassword(e.target.value)
+            }}
+
+              value={password}
+              required />
+            <button>Sign In</button>
+          </form>
+          <p>Need an Account? <br /> <span className="line">{
+            <a href="#">Sign Up</a>
+          }</span></p>
+        </div>
+      )}
+    </>)
 }
 
 export default LoginPage;
--- frontend/src/Components/Map.js
+++ frontend/src/Components/Map.js
@@ -1,28 +1,84 @@
 import React from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-import "../App.css"
-const center = { lat: 48.8584, lng: 2.2945 };
-
-
+import {
+  GoogleMap,
+  useLoadScript,
+  MarkerF,
+  Autocomplete,
+  DirectionsRenderer
+} from "@react-google-maps/api";
+import { useState, useRef } from "react";
+import "../App.css";
+const center = { lat: 43.6532, lng: -79.347 };
 
 function Map() {
-
   // const[centre, setCentre] = useState(); SET CENTRE BASED ON BUTTON
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries: ["places"],
   });
+
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null));
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
+
+  const originRef = useRef()
+
+  const destinationRef = useRef()
 
   if (!isLoaded) {
     return <h1>Loading...</h1>;
-    
   }
+
+  async function calculateRoute() {
+    if (originRef.current.value === "" || destinationRef.current.value === ""){
+      return
+    }
+    //eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService()
+    const results = await directionsService.route({
+      origin: originRef.current.value, 
+      destination: destinationRef.current.value,
+    //eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.BICYCLING
+    })
+    setDirectionsResponse(results)
+    setDistance(results.routes[0].legs[0].distance.text)
+    setDuration(results.routes[0].legs[0].duration.text)
+
+  }
+
+  function clearRoute(){
+    setDirectionsResponse(null)
+    setDistance("")
+    setDuration("")
+    originRef.current.value = ""
+    destinationRef.current.value = ""
+  }
+
   return (
     <div>
+      <button onClick={() => map.panTo(center)}>Align back to center</button>
+      <button onClick = {calculateRoute}>Calculate Route</button>
+      <button onClick = {clearRoute}>Clear Route</button>
+      <Autocomplete>
+        <input type="text" placeholder="Origin" ref={originRef} />
+      </Autocomplete>
+      <Autocomplete>
+        <input type="text" placeholder="Destination" ref= {destinationRef}/>
+      </Autocomplete>
+      <h2>Distance: {distance}</h2>
+      <h2>Duration: {duration}</h2>
       <GoogleMap
         zoom={15}
-        center={{ lat: 44, lng: -80 }}
+        center={center}
         mapContainerClassName="map-container"
-      ></GoogleMap>
+        mapTypeId="roadmap"
+        onLoad={(map) => setMap(map)}
+      >
+        <MarkerF position={center} />
+        {directionsResponse && <DirectionsRenderer directions={directionsResponse}/>}
+      </GoogleMap>
     </div>
   );
 }
